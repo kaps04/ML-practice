fbbbfc41269d5c5005ad656d289b608b4bae3b86
Kalpana Acharya <108619365+kaps04@users.noreply.github.com>
2024-07-08 21:21:25 +0545

    generators in python

---
+++ gen.py
@@ -0,0 +1,70 @@
+import men_profile
+import random 
+import time
+
+names =["John","Corey","Adam","Steve","Rick","Thomas"]
+majors =["Math", "Enginnering","CompiSci","Arts","Business"]
+
+print("memory(Before):{Mb}".format(men_profile.memory_usage_resource()))
+
+def people_list(num_people):
+  result=[]
+  for i in range(num_people):
+    person={
+      "id":i,
+      "name":random.choice(names),
+      "major":random.choice(majors)
+    }
+    result.append(person)
+  return result
+
+def people_generator(num_people):
+  for i in xrange(num_people):
+      person = {  "id":i,
+      "name":random.choice(names),
+      "major":random.choice(majors)
+        
+      }
+      yield person
+t1=time.clock()
+people = people_list(100000)
+t2 = time.clock()   
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def square_numbers(nums):
+  for i in nums:
+    yield (i*i)
+
+my_nums=square_numbers([1,2,3,4,5])
+
+
+for num in my_nums:
+  print (num)   
+  #print(list(my_nums))
+  
+print(my_nums)
